1d248aae885540020ddbd241254d6ee291ac007b
li <limw@w.cn>
2012-09-20 17:57:01 +0800

    123

--- src/li/aop/demo/T.java
+++ src/li/aop/demo/T.java
@@ -1,16 +1,27 @@
 package li.aop.demo;
 
-import li.aop.AopClassLoader;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
 
-public class T {
+public class T implements Opcodes {
 	public static void main(String[] args) throws Exception {
-		User user = AopClassLoader.newInstance(User.class);
-		user.sayHi();
-		user.sayHi();
-		user.sayHi();
+//		User user = AopClassLoader.newInstance(User.class);
+//		user.sayHi();
+//
+//		user.setAge(123);
+//		System.out.println(user.getAge());
 
-		user.setAge(123);
+		ClassNode classNode = new ClassNode();
+		ClassReader classReader = new ClassReader(User.class.getName());
+		// 注意classReader.accept的位置，必须为在填充了Node信息之前，如之后就得不到本来类中的信息。
+		classReader.accept(classNode, 0);
+		
+		System.out.println(classNode.invisibleAnnotations);
+	}
 
-		System.out.println(user.getAge());
+	public static void main2(String[] args) {
+
 	}
 }
