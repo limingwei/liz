1a0ea8f1680fa2bb35e8a86ed6ed521db2fde936
li <limw@w.cn>
2012-09-21 15:29:03 +0800

    123

--- test/li/aop/User.java
+++ test/li/aop/User.java
@@ -7,8 +7,8 @@ import li.annotation.Trans;
 @Bean
 public class User {
 
-	@Aop(LogFilter.class)
 	@Trans
+	@Aop(LogFilter.class)
 	public String sayHi(String msg1, String msg2) {
 		System.out.println("user say hi");
 		return msg1 + "\t" + msg2;
